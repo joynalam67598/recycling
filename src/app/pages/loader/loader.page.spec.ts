7d43799ee2d57e1ea006cee728583f8db56b2e7a
joynal_alam <joynal.alam@enosisbd.com>
2023-05-06 15:54:56 +0600

    add navigation over the app with TDD.

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,17 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoaderPage } from './loader.page';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app.routes';
+import { nextTick } from 'process';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [LoaderPage],
+      imports: [IonicModule.forRoot(), AppRoutingModule],
+    }).compileComponents();
     fixture = TestBed.createComponent(LoaderPage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go to login in page after load', () => {
+    spyOn(router, 'navigate');
+    component.ngOnInit();
+    // expect(component).toBeTruthy();
+    expect(router.navigate).toHaveBeenCalledWith(['login']);
   });
 });
